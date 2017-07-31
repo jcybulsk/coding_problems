b6832b5f6dd2288d38b85b64bf3bc92ccebac4e9
Ryan Cybulski <jcybulsk@users.noreply.github.com>
2017-07-31 16:07:31 -0400

    Create picking_numbers.py

---
+++ picking_numbers.py
@@ -0,0 +1,22 @@
+#!/bin/python
+import sys
+
+def max_to_choose(arr):
+    # Make a list (initially of zeros) to store the number 
+    # of integers of that number *plus* the integers one 
+    # number above. Then, after checking all the numbers in 
+    # the input array, and adding one in the appropriate 
+    # indices, I simply return the maximum count.
+    counts = [0]*99
+    for i in range(len(arr)):
+        if arr[i] > 98:
+            counts[98] += 1
+        else:
+            counts[arr[i]-1]+=1
+            counts[arr[i]]+=1
+    return max(counts)
+
+n = int(raw_input().strip())
+a = map(int,raw_input().strip().split(' '))
+
+print max_to_choose(a)
